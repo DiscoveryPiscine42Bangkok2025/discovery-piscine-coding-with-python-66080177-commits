367aaca2f33bc827338cdf6ba4c10e72fd2c40a2
pmongkol <66080177@kmitl.ac.th>
2025-09-17 02:39:51 +0700

    Update calculator.py

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,10 +1,11 @@
 x = (input("Give me the first number : "))
 y = (input("Give me the second number : "))
-x = float(x)
-y = float(y)
+x = int(x)
+y = int(y)
 print("Thank you!")
 print(f"{x} + {y} = {x + y}")
 print(f"{x} - {y} = {x - y}")
-print(f"{x} / {y} = {x / y}")
+print(f"{x} / {y} = {x // y}")
 print(f"{x} * {y} = {x * y}")
+
 
