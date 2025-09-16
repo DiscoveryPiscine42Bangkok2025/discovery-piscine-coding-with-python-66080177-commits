d08a8849c80ae0a97b378c47e2016d30f441bcc8
pmongkol <66080177@kmitl.ac.th>
2025-09-17 01:00:04 +0700

    Update advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,1 +1,11 @@
+import sys  
 
+if len(sys.argv) > 1:
+    print("none")
+else:
+    
+    for i in range(11):  
+        print("Table de", i, end=": ")
+        for j in range(11):  
+            print(i * j, end=" ")
+        print()  
