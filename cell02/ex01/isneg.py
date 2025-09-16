cf38196d03f06a008e57c59d11894ebd68289ec3
pmongkol <66080177@kmitl.ac.th>
2025-09-16 16:37:04 +0700

    Update isneg.py

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,1 +1,11 @@
-
+def main():
+    try:
+        number = float(input("Enter a number: "))
+        if number == 0:
+            print("The number is zero.")
+        else:
+            print("the number is not zero.")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+if __name__=="__main__":
+    main()
