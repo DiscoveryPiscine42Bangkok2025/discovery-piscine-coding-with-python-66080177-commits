bc515b7ac34ff2319aa7111e08858debfa1e19d2
pmongkol <66080177@kmitl.ac.th>
2025-09-16 18:56:43 +0700

    Update password.py

--- cell02/ex02/password.py
+++ cell02/ex02/password.py
@@ -1,1 +1,12 @@
+def main():
+    password = "Python is awesome"
+    user_input = input("Enter the password: ")
+    
+    if user_input == password:
+        print("ACCESS GRANTED")
+    else:
+        print("ACCESS DENIED")
+
+if __name__ == "__main__":
+    main()
 
