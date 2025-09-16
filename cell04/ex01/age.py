5df6c4a0a4efbed1af0a0371a225e1b22fcf1cc6
pmongkol <66080177@kmitl.ac.th>
2025-09-17 01:53:23 +0700

    Update age.py

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,1 +1,7 @@
+age = input("Please tell me your age : ")
 
+age = int(age)
+print("You are currently", age, "years old.")
+print("In 10 years, you'll be", age + 10, "years old.")
+print("In 20 years, you'll be", age + 20, "years old.")
+print("In 30 years, you'll be", age + 30, "years old.")
