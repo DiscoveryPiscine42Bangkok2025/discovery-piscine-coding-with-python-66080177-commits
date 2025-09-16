40df1bf855b6cdd8a94a6f414a48b3568bdaed34
pmongkol <66080177@kmitl.ac.th>
2025-09-16 18:56:07 +0700

    Update isneg.py

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,11 +1,7 @@
-def main():
-    try:
-        number = float(input("Enter a number: "))
-        if number == 0:
-            print("The number is zero.")
-        else:
-            print("the number is not zero.")
-    except ValueError:
-        print("Invalid input. Please enter a valid number.")
-if __name__=="__main__":
-    main()
+x = int(input())
+if x < 0:
+    print("This number is negative.")
+elif x == 0:
+    print("This number is both positive and negative.")
+else:
+    print("This number is positive.")
