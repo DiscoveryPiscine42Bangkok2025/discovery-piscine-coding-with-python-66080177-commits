6bfa8bf977fc3770aa500597fbca262efd59520a
pmongkol <66080177@kmitl.ac.th>
2025-09-16 18:57:13 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,1 +1,21 @@
+ef main():
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        result = num1 * num2
+
+        print(f"{num1} x {num2} = {result}")
+
+        if result > 0:
+            print("The result is positive.")
+        elif result < 0:
+            print("The result is negative.")
+        else:
+            print("The result is positive and negative.")
+    except ValueError:
+        print("Invalid input. Please enter valid numbers.")
+
+if __name__ == "__main__":
+    main()
+
 
