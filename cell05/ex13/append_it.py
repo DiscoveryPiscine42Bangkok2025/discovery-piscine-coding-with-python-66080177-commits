944274fd2678e08978f4cc366ddb5a1d58609f35
pmongkol <66080177@kmitl.ac.th>
2025-09-17 14:14:22 +0700

    Update append_it.py

--- cell05/ex13/append_it.py
+++ cell05/ex13/append_it.py
@@ -1,1 +1,17 @@
+import sys
 
+params = sys.argv[1:]
+output = []
+
+if not params:
+    print("none")
+else:
+    for word in params:
+        if not word.endswith("ism"):
+            output.append(word + "ism")
+
+    if output:
+        for item in output:
+            print(item)
+    else:
+        print("none")
