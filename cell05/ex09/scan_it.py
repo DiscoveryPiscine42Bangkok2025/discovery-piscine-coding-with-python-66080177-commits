a4ca65c8436c30d7a543043ce149e09c5a8e099b
pmongkol <66080177@kmitl.ac.th>
2025-09-17 13:20:11 +0700

    Update scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,1 +1,15 @@
+import sys
+import re
 
+if len(sys.argv) != 3:
+    print("none")
+else:
+    keyword = sys.argv[1]
+    text = sys.argv[2]
+
+    count = text.count(keyword)
+
+    if count > 0:
+        print(count)
+    else:
+        print("none")
