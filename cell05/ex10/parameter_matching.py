d5b909813ea94c744b41c008d9981325c1338c23
pmongkol <66080177@kmitl.ac.th>
2025-09-17 10:33:21 +0700

    Update parameter_matching.py

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,12 @@
+import sys
 
+if len(sys.argv) != 2:
+    print("none")
+else:
+    parameter = sys.argv[1]
+    matching = input("What was the parameter? :")
+
+    if matching == parameter:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
