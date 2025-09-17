b885bd082920228e4b78bf8633d3baed42ca7d8e
pmongkol <66080177@kmitl.ac.th>
2025-09-17 13:40:02 +0700

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,1 +1,8 @@
-
+original = [2, 8, 9, 48, 8, 22, -12, 2]
+arrays = list(set(original))
+new = []
+for i in range(len(arrays)):
+    if arrays[i] not in [2,-12]:
+        new.append(arrays[i]+2)
+print(original)
+print(set(new))
