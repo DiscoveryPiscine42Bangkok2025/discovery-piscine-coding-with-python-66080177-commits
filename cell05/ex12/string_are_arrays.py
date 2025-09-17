ff366d5bb9abcb53e83db05bbaa246f3e753c91a
pmongkol <66080177@kmitl.ac.th>
2025-09-17 12:21:42 +0700

    Update string_are_arrays.py

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,1 +1,13 @@
+import sys
 
+if len(sys.argv) != 2:
+    print("none")
+else:
+    parameters = sys.argv[1]  
+
+    count = parameters.count("z")
+
+    if count == 0:
+        print("none")
+    else:
+        print("z" * count)
