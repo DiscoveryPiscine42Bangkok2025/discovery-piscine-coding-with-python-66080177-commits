a51c2fe852607688024adedf72fcb0495504fecf
pmongkol <66080177@kmitl.ac.th>
2025-09-17 19:57:48 +0700

    Update free_range.py

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,1 +1,15 @@
+import sys
 
+params = sys.argv[1:]
+
+if len(params) != 2:
+    print("none")
+else:
+    try:
+        start = int(params[0])
+        end = int(params[1])
+
+        result = list(range(start, end + 1))
+        print(result)
+    except ValueError:
+        print("none")
